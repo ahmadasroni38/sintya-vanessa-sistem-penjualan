6d5b04c6c801418f83ca41ceef029f7656c2d1ea
Ahmad Asroni <ahmadasroni38@gmail.com>
2026-01-12 10:24:29 +0800

    oke

--- resources/js/components/Locations/LocationFormModal.vue
+++ resources/js/components/Locations/LocationFormModal.vue
@@ -47,49 +47,14 @@
                         :error="getFieldError('color')"
                         @blur="handleFieldBlur('color')"
                     />
-                    <!-- Parent Location Select -->
-                    <div class="relative">
-                        <label
-                            class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1"
-                        >
-                            {{ $t('locations.parentLocation') }}
-                        </label>
-                        <select
-                            v-model="formData.parent_id"
-                            data-hs-select='{
-                              "hasSearch": true,
-                              "searchPlaceholder": "Search parent locations...",
-                              "searchClasses": "block w-full sm:text-sm border-gray-200 rounded-lg focus:border-red-500 focus:ring-red-500 before:absolute before:inset-0 before:z-1 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 py-1.5 sm:py-2 px-3",
-                              "searchWrapperClasses": "bg-white p-2 -mx-1 sticky top-0 dark:bg-neutral-900",
-                              "placeholder": "Select parent location (optional)",
-                              "toggleTag": "<button type=\"button\" aria-expanded=\"false\"><span class=\"me-2\" data-icon></span><span class=\"text-gray-800 dark:text-neutral-200 \" data-title></span></button>",
-                              "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 ps-4 pe-9 flex gap-x-2 text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:outline-hidden focus:ring-2 focus:ring-red-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:outline-hidden dark:focus:ring-1 dark:focus:ring-neutral-600",
-                              "dropdownClasses": "mt-2 max-h-72 pb-1 px-1 space-y-0.5 z-20 w-full bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:bg-neutral-900 dark:border-neutral-700",
-                              "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:text-neutral-200 dark:focus:bg-neutral-800",
-                              "optionTemplate": "<div><div class=\"flex items-center\"><div class=\"me-2\" data-icon></div><div class=\"text-gray-800 dark:text-neutral-200 \" data-title></div></div></div>",
-                              "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"shrink-0 size-3.5 text-gray-500 dark:text-neutral-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
-                            }'
-                            class="hidden"
-                            @blur="handleFieldBlur('parent_id')"
-                        >
-                            <option value="">
-                                {{ $t('locations.selectParentPlaceholder') }}
-                            </option>
-                            <option
-                                v-for="parent in parentOptions"
-                                :key="parent.value"
-                                :value="parent.value"
-                            >
-                                {{ parent.label }}
-                            </option>
-                        </select>
-                        <p
-                            v-if="getFieldError('parent_id')"
-                            class="mt-1 text-sm text-red-600 dark:text-red-400"
-                        >
-                            {{ getFieldError("parent_id") }}
-                        </p>
-                    </div>
+                    <FormSelect
+                        v-model="formData.parent_id"
+                        :label="$t('locations.parentLocation')"
+                        :options="parentOptions"
+                        :placeholder="$t('locations.selectParentPlaceholder')"
+                        :error="getFieldError('parent_id')"
+                        @blur="handleFieldBlur('parent_id')"
+                    />
                 </div>
             </div>
 
@@ -140,7 +105,7 @@
                         @blur="handleFieldBlur('postal_code')"
                     />
                 </div>
-                <div class="grid grid-cols-2 gap-4">
+                <div class="grid grid-cols-2 gap-4 hidden">
                     <FormInput
                         v-model="formData.latitude"
                         :label="$t('locations.latitude')"
@@ -195,6 +160,7 @@ import { ref, computed, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import Modal from "../Overlays/Modal.vue";
 import FormInput from "../Forms/FormInput.vue";
+import FormSelect from "../Forms/FormSelect.vue";
 import FormTextarea from "../Forms/FormTextarea.vue";
 import Button from "../Base/Button.vue";
 
